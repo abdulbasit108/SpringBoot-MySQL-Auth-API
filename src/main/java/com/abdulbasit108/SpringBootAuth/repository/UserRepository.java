f752978d9bae0fe4fdbfaa1ac54d2b7ecb7bcfad
Abdul Basit <abdbasit108@gmail.com>
2024-08-31 00:52:08 +0500

    first commit

---
+++ src/main/java/com/abdulbasit108/SpringBootAuth/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.abdulbasit108.SpringBootAuth.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.abdulbasit108.SpringBootAuth.model.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationCode(String verificationCode);
+}
---
+++ src/test/java/com/abdulbasit108/SpringBootAuth/SpringBootAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulbasit108.SpringBootAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
